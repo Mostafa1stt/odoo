f8e9b118f7a19768d422873e74224296e5a33483
Mostafa1stt <mostafa1stt@gmail.com>
2025-08-06 15:11:29 +0300

    odoo

---
+++ odoo17/odoo/custom_addons/app_one/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "ToDo App",
+    'author': "mostafa abdelkareem",
+    'category': "",
+    'version': "17.0.0.1.0",
+    'depends': ['base',],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menu.xml',
+        'views/task_view.xml',
+        'views/user_view.xml',
+    ],
+    'application': True,
+
+}
